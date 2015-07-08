93954d889b4ed47362a62b926acacdc0a5d254d9
Khairil Yusof <khairil.yusof@gmail.com>
2015-07-08 19:05:54 +0800

    Initial vrsion of mp lookup vocab. Vocab term generator not added yet.

--- sinar/representatives/vocabulary/mp.py
+++ sinar/representatives/vocabulary/mp.py
@@ -4,19 +4,34 @@ from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 from zope.schema.interfaces import IVocabularyFactory
 from zope.component import getUtility
 from z3c.formwidget.query.interfaces import IQuerySource
+from zope.interface import implements, alsoProvides
+from zope.schema.interfaces import ITitledTokenizedTerm
+
+import json
+import requests
 
 class mp(grok.GlobalUtility):
+    """A queriable source for MPs.
+    popit_ids are tokens and also values, title is name.
+    """
     grok.name('sinar.representatives.mp')
-    grok.implements(IVocabularyFactory)
 
-    _terms = [{
-        'value': 'termvalue',
-        'title': 'Term Title',
-        'token': 'termtoken',
-    }]
+    implements(IQuerySource)
 
-    def __call__(self, context):
-        terms = []
-        for i in self._terms:
-            terms.append(SimpleTerm(*i))
-        return SimpleVocabulary(terms)
+    def __init__(self):
+        pass
+    
+    def search(self, query_string):
+
+        seen = set()
+
+        popit_request = requests.get('http://sinar-malaysia.popit.mysociety.org/api/v0.1/search/persons?q=name:'
+                + query_string)
+        
+        results = json.loads(popit_request.content)['result']
+
+        for result in results:
+            seen.add(result['id'])
+            token = result['id']
+            value = result['id']
+            title = result['name']
